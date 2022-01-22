d556d2b61eb7874832ddf4849cee1487f574ad04
Francesco Drudi <fdrudi@e3r3p5.42roma.it>
2022-01-22 15:09:53 +0100

    Base_Function

--- 42_next_line/get_next_line.c
+++ 42_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fdrudi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 14:14:18 by fdrudi            #+#    #+#             */
-/*   Updated: 2022/01/21 16:54:49 by fdrudi           ###   ########.fr       */
+/*   Updated: 2022/01/22 15:09:18 by fdrudi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,110 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include "get_next_line.h"
+#include <stddef.h>
 
-char	*get_next_line(int fd)
+
+size_t	ft_strlen(const char *s)
 {
-	char	buf[BUFFER_SIZE + 1];
-	char	*dst;
-	size_t	file_len;
 	size_t	i;
-	size_t	x;
-	size_t	count;
+	
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*united;
+	int		i;
+	int		j;
 
 	i = 0;
-	count = 0;
-	while (file_len != 0)
+	j = 0;
+
+	if (s2 == NULL)
+		return (NULL);
+
+	united = (char *) malloc (ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (united == NULL)
+		return (NULL);
+	while (s1[j] != '\0')
+		united[i++] = s1[j++];
+	j = 0;
+	while (s2[j] != '\0')
+		united[i++] = s2[j++];
+	united[i] = '\0';
+	printf("%s\n", united);
+	return (united);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = ft_strlen(s) + 1;
+	while (i--)
+	{
+		if (*s == (char) c)
+			return ((char *) s);
+		s++;
+	}
+	return (0);
+}
+
+char	*read_line(int fd)
+{
+	char	buf[BUFFER_SIZE + 1];
+	size_t	file_len;
+	char	*tmp;
+
+	file_len = 1;
+	tmp = malloc (1);
+	tmp[0] = '\0';
+	while (file_len != 0 && ft_strchr(buf, '\n') == 0)
 	{
 		file_len = read(fd, buf, BUFFER_SIZE);
-		count += file_len;
+		buf[file_len] = '\0';
+		tmp = ft_strjoin(tmp, buf);
 	}
-	//while (buf[i - 1] != '\n' && buf[i] != '\0' && i < count)
-	//	i++;
-	printf("%zu", count);
+	printf("%s\n", tmp);
+	return (tmp);
+}
 
-	dst = (char *) malloc (count + 1);
-	if (!dst)
+char	*define_line(char *dst)
+{
+	int		i;
+	int		x;
+	char	*new;
+
+	i = 0;
+	while (dst[i - 1] != '\n' && dst[i] != '\0')
+		i++;
+	new = (char *) malloc (i + 1);
+	if (!new)
 	return (NULL);
 	x = 0;
 	while (i--)
 	{
-		dst[x] = buf[x];
+		new[x] = dst[x];
 		x++;
 	}
-	dst[x] = '\0';
-	printf("%s", dst);
-	return (dst);
+	new[x] = '\0';
+	printf("%s\n", new);
+	return (new);
+
+}
+
+char	*get_next_line(int fd)
+{
+	char	*dst;
+	char	*new;
+//	static int	count;
+
+	dst = read_line(fd);
+	new = define_line(dst);
+	return (new);
 }
 
 
@@ -56,6 +127,7 @@ int main()
 	int fd;
 
 	fd = open("test.txt", O_RDONLY);
+	printf("%d\n", fd);
 	get_next_line(fd);
 	return (0);
 }
